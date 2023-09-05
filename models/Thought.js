5e7402f95fa2bc05de6ff0e6207c8518591398ad
alexeflorin <goremic@yahoo.com>
2023-09-04 23:31:23 -0400

    fix models

---
+++ models/Thought.js
@@ -0,0 +1,87 @@
+const { Schema, model, Types } = require('mongoose');
+
+const { DateTime } = require("luxon");
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        // Use Mongoose's ObjectId data type
+        default: new Types.ObjectId(),
+        // Default value is set to a new ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        // max length of 280 characters
+    },
+    username: {
+        type: String,
+        required: true,
+        // username is required
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => createdAtVal.toDateString(),
+        // date and time of when the reaction was created
+    },
+
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+
+});
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            // max length of 280 characters
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => createdAtVal.toDateString(),
+            // date and time of when the reaction was created
+
+        },
+        username: {
+            type: String,
+            required: true,
+            // username is required
+        },
+        reactions: [ReactionSchema],
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+    }
+);
+
+
+userSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+// create a new instance of the Thought model using the ThoughtSchema
+
+
+
+
+
+module.exports = Thought;
+
+// export the Thought model
+
+
---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match a valid email address!'],
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+
+});
+
+// User model will use the UserSchema
+const User = model('User', userSchema);
+
+// export the User model
+module.exports = User;
